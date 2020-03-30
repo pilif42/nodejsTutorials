76a62df6c44f4bf907273b8ed3cf9523eb126393
Philippe Brossier <brossierp@gmail.com>
2020-03-30 17:27:39 +0100

    1st node js app done

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Starting point = https://www.w3schools.com/nodejs
 # To start the Node.js app
 ##################################################
 cd /home/philippe/code/nodejsTutorials
-node myfirst.js
+node myfirstapp.js
 
 
 ##################################################
 # To test the Node.js app
 ##################################################
-Go to http://localhost:8080/mytest
+Go to http://localhost:8080/mytest?year=2017&month=July
--- myfirstapp.js
+++ myfirstapp.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var url = require('url');
 var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+
+  var query = url.parse(req.url, true).query;
+  var querytxt = query.year + " " + query.month;
+
   res.write("The url is: " + req.url + "<br/>");
+  res.write("As query params, we have a year and a month retrospectively: " + querytxt + "<br/>");
   res.write("The date and time are currently: " + dt.myDateTime());
   res.end();
 }).listen(8080);
